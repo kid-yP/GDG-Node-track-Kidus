c26b5dc2b46779104776f25f0273d2dffd6cfa84
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-01-17 11:37:37 +0300

    Update app.js

--- Week4/src/app.js
+++ Week4/src/app.js
@@ -1,1 +1,16 @@
+import express from "express";
+import userRoutes from "./routes/userRoutes.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
+const app = express();
+
+//Middleware
+app.use(express.json());
+
+//Routes
+app.use("/users", userRoutes);
+
+//error handling
+app.use(errorHandler);
+
+export default app;
