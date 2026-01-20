a3e1499ec05e070ec5890e1928287782838e4232
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-01-20 06:23:49 +0300

    Add user routes for CRUD operations

--- Week4/src/routes/userRoutes.js
+++ Week4/src/routes/userRoutes.js
@@ -1,1 +1,17 @@
+import express from "express";
+import * as userController from "../controllers/userController.js";
 
+const router = express.Router();
+
+//get all users
+router.get("/", userController.getAllUser);
+//get user by ID
+router.get("/:id", userController.getUserById);
+//Create a new user
+router.post("/", userController.createNewUser);
+// Update a user by ID
+router.put("/:id", userController.updateUserById);
+// Delete a user by ID
+router.delete("/:id", userController.deleteUserById);
+
+export default router;
