c8fb5a19f2bf82754782da8c9f68b5ae1c13185a
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-01-21 06:13:51 +0300

    Update errorHandler.js

--- Week4/src/middleware/errorHandler.js
+++ Week4/src/middleware/errorHandler.js
@@ -1,1 +1,10 @@
+const errorHandler = (err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).json({
+        success: "false",
+        message: "Something went wrong",
+        error: err.message,
+    });
+};
 
+export default errorHandler;
